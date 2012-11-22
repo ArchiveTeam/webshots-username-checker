066d89934f73900bddb3a460fce5ebd2905b25ea
Alard <alard@example.com>
2012-11-22 13:44:09 +0100

    First version.

---
+++ pipeline.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from seesaw.externalprocess import *
+from seesaw.pipeline import *
+from seesaw.project import *
+
+pipeline = Pipeline(
+    ExternalProcess("WebshotsChecker", ["./check-webshots.py"])
+)
+
+project = Project(
+    title = "Webshots checker",
+    project_html = """
+    <h2>Webshots checker</h2>
+    <p>Help to discover valid Webshots usernames.</p>
+    """
+)
